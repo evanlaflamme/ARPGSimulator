23331f64c00bdfd162e1797766590f16806f2d36
evanlaflamme <evanlaflamme@hotmail.com>
2017-03-10 22:04:29 -0500

    Add player collider boxes

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,16 +4,31 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-	public Vector3 targetPosition;
+	private Vector3 targetPosition;
+	public int health;
+	public int mana;
 
 	// Update is called once per frame
 	void Update () {
-
 		if (Input.GetKeyDown(KeyCode.Mouse0))
 		{
 			targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 		}
+		if (Input.GetKeyDown(KeyCode.Mouse1))
+		{
+			print ("Cleave.");
+		}
+		if (Input.GetKeyDown(KeyCode.Q)) {
+			print("Bash.");
+		}
+		if (Input.GetKeyDown(KeyCode.W)) {
+			print("Dash.");
+		}
 
 		transform.position = Vector3.MoveTowards(transform.position, targetPosition, Time.deltaTime * 5);
+	}
+
+	public void damage(int dmg) {
+		health = health - dmg;
 	}
 }
