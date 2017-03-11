64e980c34520b57f1770f26ee1d58cee97461ba3
MrL3gume <justin.tremblay@mail.mcgill.ca>
2017-03-11 11:54:33 -0500

    Worked on colliders, ennemy death, etc.

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class EnemyController : MonoBehaviour {
 
 	public GameObject player;
+	public Animator anim;
 
-	private const int MAX_HEALTH = 1000;
+	private const int MAX_HEALTH = 200;
 	public int health = MAX_HEALTH;
+	public bool isDead = false;
 
 	private Vector2 targetPosition;
 
@@ -18,12 +20,30 @@ public class EnemyController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		targetPosition = player.transform.position;
+		if (!isDead) {
+			targetPosition = player.transform.position;
+			transform.position = Vector3.MoveTowards (transform.position, targetPosition, Time.deltaTime * 0.15F);
+		}
 
-		transform.position = Vector3.MoveTowards(transform.position, targetPosition, Time.deltaTime * 0.35F);
+		updateAnimator ();
 	}
 
 	void damage(int dmg) {
 		health = health - dmg;
+		if (health <= 0)
+			isDead = true;
 	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.gameObject.CompareTag ("Cleave")) {
+			PlayerController source = other.gameObject.GetComponentInParent<PlayerController>();
+			damage (source.doDamage(1.0f));
+		}
+	}
+
+	private void updateAnimator() {
+		//anim.SetFloat ("Speed", Mathf.Abs(rb.velocity.magnitude));
+		anim.SetBool("isDead", isDead);
+	}
+
 }
