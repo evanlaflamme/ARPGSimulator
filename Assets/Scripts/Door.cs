a95c6a6a7a1dfdf7db59788ba3220b26ea5f3d2c
evanlaflamme <evanlaflamme@hotmail.com>
2017-03-12 12:21:24 -0400

    Clean up project a bit more

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -5,23 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class Door : MonoBehaviour {
 
-	public Animator anim;
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
 	void OnTriggerEnter2D (Collider2D other) {
-		if (other.gameObject.CompareTag ("Player")) {
-			anim.SetBool ("open", true);
-			//anim.Play("Open");
-		}
 		GameManager gm = GameObject.FindGameObjectWithTag ("GameController").GetComponent<GameManager>();
 		gm.nextLevel ();
 	}
